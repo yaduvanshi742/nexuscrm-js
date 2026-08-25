2efeb5841c4b60661029709a4dc0efb5bcdcbc19
Yadhuvanshi <yaduvanshi742@proton.me>
2026-08-25 13:16:23 +0530

    Add utility functions for formatting money, date, and text

---
+++ client/src/utils/format.js
@@ -0,0 +1,29 @@
+export function money(value) {
+  return new Intl.NumberFormat('en-IN', {
+    style: 'currency',
+    currency: 'INR',
+    maximumFractionDigits: 0
+  }).format(Number(value || 0));
+}
+
+export function date(value) {
+  if (!value) return 'No date';
+  return new Intl.DateTimeFormat('en-IN', {
+    day: '2-digit',
+    month: 'short',
+    year: 'numeric'
+  }).format(new Date(value));
+}
+
+export function titleCase(value = '') {
+  return String(value).replace(/-/g, ' ').replace(/\b\w/g, (char) => char.toUpperCase());
+}
+
+export function escapeHtml(value = '') {
+  return String(value)
+    .replaceAll('&', '&amp;')
+    .replaceAll('<', '&lt;')
+    .replaceAll('>', '&gt;')
+    .replaceAll('"', '&quot;')
+    .replaceAll("'", '&#039;');
+}

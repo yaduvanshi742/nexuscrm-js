214fdd4d5a6f0227c6c938c0e559aeddb42cbf6e
Yadhuvanshi <yaduvanshi742@proton.me>
2026-09-04 13:18:37 +0530

    Add constants for deal stages, task statuses, and roles

---
+++ server/src/config/constants.js
@@ -0,0 +1,5 @@
+export const DEAL_STAGES = ['lead', 'qualified', 'proposal', 'negotiation', 'won', 'lost'];
+
+export const TASK_STATUSES = ['open', 'in-progress', 'done'];
+
+export const ROLES = ['admin', 'manager', 'member'];
